9b5ac2bb67f6cc43f850fa1dbdf033753beb5324
BrunoBononi <41966707+BrunoBononi@users.noreply.github.com>
2018-08-03 18:40:08 -0300

    sua alteracao

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,18 +2,21 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { ViewChild } from '@angular/core';
+import { Slides } from 'ionic-angular';
 
 import { HomePage } from '../pages/home/home';
 @Component({
   templateUrl: 'app.html'
 })
+
+//Chama a primeira página
 export class MyApp {
   rootPage:any = HomePage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
+      
       statusBar.styleDefault();
       splashScreen.hide();
     });
--- src/app/main.ts
+++ src/app/main.ts
@@ -1,5 +1,6 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+import { ViewChild } from '@angular/core';
+import { Slides } from 'ionic-angular';
 import { AppModule } from './app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule);
